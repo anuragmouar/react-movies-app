1cc54880010d809e3222178165a14cff809badcc
= <=>
2020-04-25 20:17:30 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/home/Home';
 
-ReactDOM.render(<div></div>>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<Home />, document.getElementById('root'));
 serviceWorker.unregister();
